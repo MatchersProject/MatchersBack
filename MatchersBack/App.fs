c10444f7f03fc8851e709ce52c6c3ce4745daf0d
Amram ELBAZ <a.elbaz@adexgroup.fr>
2018-07-13 15:08:55 +0200

    Initiate project

---
+++ MatchersBack/App.fs
@@ -0,0 +1,11 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+let game =
+    choose [
+        GET >=>  OK (View.index)
+    ]
+
+startWebServer defaultConfig game
---
+++ MatchersBack/View.fs
@@ -0,0 +1,30 @@
+﻿module View  
+open System
+
+open Suave.Html
+open Suave.Form
+open Form
+    
+
+let index =
+    html [] [
+        head [] [
+            title [] "Matchers"
+        ]
+
+        body [] [
+            div ["id", "header"] [
+                tag "h1" [] [
+                    a "/" [] [Text "Matchers"]
+                ]
+            ]
+
+            div ["id", "footer"] [
+                Text "built with "
+                a "http://fsharp.org" [] [Text "F#"]
+                Text " and "
+                a "http://suave.io" [] [Text "Suave.IO"]
+            ]
+        ]
+    ]
+    |> htmlToString
